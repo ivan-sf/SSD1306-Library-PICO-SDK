4538698a648a93641b36eebcb65bf0b6d28c991f
ivan-sf <sf.ivan2023@gmail.com>
2025-03-13 00:39:10 -0300

    Update SSD1306_Exemplo.c

--- SSD1306_Exemplo.c
+++ SSD1306_Exemplo.c
@@ -47,7 +47,7 @@ int main(){
 
     //SSD1306_DrawCircle(ssd, 64, 32, 20, SSD1306_COLOR_WHITE);
 
-    SSD1306_DrawFilledCircle(ssd, 10, 10, 5, SSD1306_COLOR_WHITE);
+    //SSD1306_DrawFilledCircle(ssd, 10, 10, 5, SSD1306_COLOR_WHITE);
 
     //SSD1306_DrawArc(ssd, 64, 32, 20, 180, 0, SSD1306_COLOR_WHITE);
 
@@ -57,7 +57,51 @@ int main(){
 
     //SSD1306_SetStringWrapped(ssd, 0, 0, "ABCD\nWXYZ", &Font_7x10, SSD1306_COLOR_WHITE);
 
+    /*SSD1306_DrawFilledRectangle(ssd, 10, 10, 90, 40, SSD1306_COLOR_WHITE);
+
     SSD1306_Render(ssd, &FrameArea);
+
+    sleep_ms(3000);
+
+    SSD1306_DrawFilledTriangle(ssd, 15, 15, 60, 15, 40, 40, SSD1306_COLOR_BLACK);
+
+    SSD1306_Render(ssd, &FrameArea);
+
+    sleep_ms(3000);
+
+    SSD1306_DrawFilledCircle(ssd, 75, 30, 10, SSD1306_COLOR_BLACK);
+
+    SSD1306_Render(ssd, &FrameArea);
+
+    sleep_ms(3000);
+
+    SSD1306_DrawArc(ssd, 75, 30, 15, 90, 270, SSD1306_COLOR_BLACK);
+
+    SSD1306_Render(ssd, &FrameArea);
+    
+    sleep_ms(1500);
+
+    SSD1306_Clear(ssd, &FrameArea);*/
+
+    SSD1306_SetChar(ssd, 0, 0, 'A', &Font_7x10, SSD1306_COLOR_WHITE);
+
+    SSD1306_Render(ssd, &FrameArea);
+
+    sleep_ms(5000);
+
+    SSD1306_SetString(ssd, 0, 15, "VIRTUS", &Font_11x18, SSD1306_COLOR_WHITE);
+
+    SSD1306_Render(ssd, &FrameArea);
+
+    sleep_ms(5000);
+
+    SSD1306_SetStringWrapped(ssd, 0, 40, "VIR\nTUS", &Font_7x10, SSD1306_COLOR_WHITE);
+
+    SSD1306_Render(ssd, &FrameArea);
+
+    sleep_ms(3000);
+
+    SSD1306_Clear(ssd, &FrameArea);
 
     while(1){
 
